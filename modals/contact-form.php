f61acca866864ce5b71e488d19bfc8b62f0e45ea
Edmand <edmand66@hotmail.com>
2025-09-16 23:00:50 +0800

    fix(contact): Improved form validation, styling and maintainability

--- modals/contact-form.php
+++ modals/contact-form.php
@@ -1,14 +1,15 @@
 <!-- Contact Form Modal -->
 <div class="modal fade" id="contactModal" tabindex="-1" aria-labelledby="contactModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="contactModalLabel">Contact Us</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <?php echo do_shortcode('[contact-form-7 id="b7996ab" title="Contact Form"]'); ?>
-      </div>
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="contactModalLabel">Contact Us</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <!-- Ensure that the title is "Contact Form", so that the id does not matter -->
+                <?php echo do_shortcode('[contact-form-7 title="Contact Form"]'); ?>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
